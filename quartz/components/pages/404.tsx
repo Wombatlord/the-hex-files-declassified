ca2e3f17fbc0a86b58bf9a2669af5c54184f5927
Wombatlord <jordan.stone0@gmail.com>
2025-07-29 02:46:59 +0100

    Quartz sync: Jul 29, 2025, 2:46 AM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -3,14 +3,13 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   // If baseUrl contains a pathname after the domain, use this as the home link
-  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}/the-hex-files-declassified`)
+  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
   const baseDir = url.pathname
 
   return (
     <article class="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
-	  <p>here is some writing</p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
